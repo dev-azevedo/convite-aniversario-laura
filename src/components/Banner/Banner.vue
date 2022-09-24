af2968db12e3adb7fd64cebd27dbe608c4f759a5
Jhonatan Azevedo <dev.azevedo@outlook.com>
2022-09-24 16:16:17 -0300

    ADD: init project

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,19 @@
+<template>
+  <section id="banner"></section>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+};
+</script>
+
+<style scoped>
+#banner {
+  width: 100%;
+  height: 100vh;
+  background-image: url("./../../assets/img/Background.png");
+  background-size: cover;
+  background-position: right;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="body-banner" class="d-flex justify-content-end bg-danger">
+      <Banner id="banner" />
+      <div id="conteudo">
+        <Header />
+        <p class="w-60">
+          Venha comemorar o primeiro aniversário da Laurinha, contamos com sua
+          presença!
+        </p>
+        <br />
+        <p>
+          <span class="fw-bold">ESPAÇO YELLOW</span>
+          <br />
+          Endereço: Rua Rio Jaguaribe, 426 - Bairro Alto - Curitiba/PR
+          <br />
+          Obs: Estacionamento atrás do salão na rua ao lado
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Navbar/Header.vue";
+import Banner from "@/components/Banner/Banner.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Banner,
+  },
+};
 </script>
+
+<style  scooped>
+#conteudo {
+  width: 60%;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center !important;
+  position: absolute;
+  left: 0;
+  color: #fff;
+  font-size: 26px;
+  display: flex;
+}
+.w-60 {
+  width: 60% !important;
+}
+#conteudo p {
+  text-align: center;
+  margin: 0 auto;
+  text-shadow: 1px 1px 1px #595959;
+}
+#body-banner {
+  position: relative;
+  top: 0;
+  z-index: 1500 !important;
+}
+</style>
